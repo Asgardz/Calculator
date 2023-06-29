3818cf493050de0671f3a19a14761e61270aa5fc
Asgardz <2366651437@qq.com>
2023-06-29 13:10:43 +0800

    点击响应

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action
         const keyContent = key.textContent
         const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
         if(!action){
+            // console.log('number key')
             // 如果计算机显示0，就把点击的数字显示上去；如果不是0，就把点击的数字加在后面
-            if(displayedNum === '0'){
+            if(displayedNum === '0' || previousKeyType === 'operator'){
                 display.textContent = keyContent
             }else{
                 display.textContent = displayedNum + keyContent
-            }
-            console.log('number key')
+            }        
         }
 
         if (
@@ -23,21 +25,26 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
           ) {
+            // console.log('operator key!')
             key.classList.add('is-deperssed')
-            console.log('operator key!')
+            calculator.dataset.previousKeyType = 'operator'   
           }
 
         if (action === 'decimal') {
+            // console.log('decimal key!')
             display.textContent = displayedNum + '.'
-            console.log('decimal key!')
         }
         
         if (action === 'clear') {
-            console.log('clear key!')
+            // console.log('clear key!')
         }
         
         if (action === 'calculate') {
-            console.log('equal key!')
+            // console.log('equal key!')
+            const secondValue = displayedNum
         }
+
+        //移除is-depressed类
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
     }
 })
